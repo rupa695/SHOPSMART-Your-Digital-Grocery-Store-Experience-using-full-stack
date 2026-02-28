cc8d9527076955800ce9eefee0f01aa2857bae6b
rupa695 <2022cse.r454@svce.edu.in>
2026-02-28 12:22:58 +0530

    Add files via upload

--- styledComponents.js
+++ styledComponents.js
@@ -1,32 +1,34 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-export const FooterContainer = styled.footer`
-  background-color: #333;
-  color: #fff;
-  padding: 20px 0;
-`;
+// export const NavBar = styled.nav`
+// background-color: ${props => (props.dark ? "#333" : "#fff")};
+// color: ${props => (props.dark ? "#fff" : "#333")};
+// display: flex;
+// justify-content: space-between;
+// align-items: center;
+// height:10vh;
+// padding:0 20px;
+// position:fixed;
+// width:100%;
+// `;
 
-export const FooterContent = styled.div`
-  max-width: 1200px;
-  margin: 0 auto;
-  text-align: center;
-`;
+// export const NavItems = styled.ul`
+// list-style: none;
+// display: flex;
+// gap: 20px;
+// margin: 0;
+// padding: 0;
+// `;
 
-export const FooterText = styled.p`
-  font-size: 14px;
-  margin: 0;
-`;
+// export const NavItem = styled.li`
+// a {
+//   color: inherit;
+//   text-decoration: none;
+//   font-weight: bold;
+//   transition: color 0.3s;
 
-export const FooterLinks = styled.div`
-  margin-top: 10px;
-`;
-
-export const FooterLink = styled.a`
-  color: #fff;
-  margin: 0 10px;
-  text-decoration: none;
-
-  &:hover {
-    text-decoration: underline;
-  }
-`;
+//   &:hover {
+//     color: #007bff;
+//   }
+// }
+// `;
