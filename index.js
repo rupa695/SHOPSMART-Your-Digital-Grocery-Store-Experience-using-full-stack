8f097d9fafe4781450ad0b4f508ae026ebab3933
rupa695 <2022cse.r454@svce.edu.in>
2026-02-28 12:32:41 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,200 +1,197 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom'; // Import useParams to get the product ID from the URL
-import styled from 'styled-components';
+import { Button, Table,Card } from 'react-bootstrap';
+import { FaTrash,FaEdit } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import AdminNavabar from '../AdminNavbar'
 
 
-// Styled components (you can adjust styles as needed)
-const Container = styled.div`
-  max-width: 700px;
-  margin: 5vh auto;
-  text-align: start;
-  background-color:skyblue;
-`;
+const Users = () => {
+  const [userbookings, setUserbookings] = useState([]);
+  const [users, setUsers] = useState([]);
 
-const Heading = styled.h2`
-  font-size: 24px;
-  font-weight: bold;
-  color: rgb(62, 62, 62);
-  margin-bottom: 20px;
-`;
+  const [showDetails, setShowDetails] = useState(false);
 
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-`;
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+  const [showDetail, setShowDetail] = useState(false);
 
-const FormGroup = styled.div`
-  margin-bottom: 20px;
-`;
-
-const Label = styled.label`
-  font-weight: bold;
-  margin-bottom: 8px;
-`;
-
-const Input = styled.input`
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  font-size: 16px;
-  width: 100%;
-`;
-
-const Textarea = styled.textarea`
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  font-size: 16px;
-  width: 100%;
-  min-height: 100px;
-`;
-
-const Button = styled.button`
-  background-color: blue;
-  color: white;
-  padding: 10px 20px;
-  border: none;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-
-  &:hover {
-    background-color: orangered;
-  }
-`;
-
-const UpdateProduct = () => {
-  const { id } = useParams(); // Get the product ID from the URL
-
-  const [formData, setFormData] = useState({
-    productname: '',
-    description: '',
-    price: '',
-    image: '',
-    category: '',
-    countInStock: '',
-    rating: '',
-  });
-
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    // Fetch the existing product data
-    axios.get(`http://localhost:5100/products/${id}`)
-      .then((response) => {
-        // Populate the form with the existing product data
-        setFormData(response.data);
-      })
-      .catch((error) => {
-        console.error('Error fetching product data:', error);
-      });
-  }, [id]);
-
-  const { productname, description, price, image, category, countInStock, rating } = formData;
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const toggleDetail = () => {
+    setShowDetail(!showDetail);
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+   useEffect(() => {
+    axios.get(`http://localhost:5100/users`)
+      .then((response) => {
+        setUsers(response.data);
+        // setLoading(false);
+      })
+      .catch((error) => {
+        // setError('Failed to fetch projects.');
+        // setLoading(false);
+      });
+}, []);
 
-    try {
-      const response = await axios.put(`http://localhost:5100/products/${id}`, formData);
+const deleteData = (taskId) => {
+    axios.delete(`http://localhost:5100/userdelete/${taskId}`);
+    window.location.assign('/admin/users');
+    alert('User is deleted');
+  };
+  const deleteorder = (taskId) => {
+    axios.delete(`http://localhost:5100/userbookingdelete/${taskId}`);
+    window.location.assign('/admin/users');
+    alert('deleted');
+  };
 
-      console.log('Product updated:', response.data);
-      navigate('/admin/all-products')
 
-      // Handle any other actions upon successful product update
+  const fetchUserBikeData = (userId) => {
+   
+    axios.get(`http://localhost:5100/getbookings/${userId}`)
 
-    } catch (error) {
-      console.error('Error updating product:', error);
-      // Handle errors here, e.g., show an error message to the user
+    .then((response) => {
+      setUserbookings(response.data);
+      toggleDetails(); // Show Plan Details when data is fetched
+    })
+    .catch((error) => {
+      console.error('Error fetching user bike data:', error);
+    });
+  };
+  const calculateStatus = (date) => {
+    const currentDate = new Date();
+    const formattedDeliveryDate = new Date(date);
+
+    if (formattedDeliveryDate >= currentDate) {
+      return "Upcomming";
+    } else {
+      return "completed";
     }
   };
 
   return (
-   <div>
-    <AdminNavabar/>
-    <h1 className='text-center'>Update Product</h1>
-     <Container>
-      <Form onSubmit={handleSubmit} className='shadow p-3'>
-        {/* Render form fields with existing data */}
-        <FormGroup>
-          <Label htmlFor="productname">Product Name</Label>
-          <Input
-            type="text"
-            name="productname"
-            value={productname}
-            onChange={handleChange}
-            placeholder="Enter product name"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label htmlFor="rating">Rating</Label>
-          <Input
-            type="number"
-            name="rating"
-            value={rating}
-            onChange={handleChange}
-            placeholder="Enter product rating"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label htmlFor="price">Price</Label>
-          <Input
-            type="number"
-            name="price"
-            value={price}
-            onChange={handleChange}
-            placeholder="Enter product price"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label htmlFor="image">Image URL</Label>
-          <Input
-            type="text"
-            name="image"
-            value={image}
-            onChange={handleChange}
-            placeholder="Enter image URL"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label htmlFor="category">Category</Label>
-          <Input
-            type="text"
-            name="category"
-            value={category}
-            onChange={handleChange}
-            placeholder="Enter category"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label htmlFor="countInStock">Count in Stock</Label>
-          <Input
-            type="number"
-            name="countInStock"
-            value={countInStock}
-            onChange={handleChange}
-            placeholder="Enter count in stock"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label htmlFor="description">Description</Label>
-          <Textarea
-            name="description"
-            value={description}
-            onChange={handleChange}
-            placeholder="Enter product description"
-          />
-        </FormGroup>
-        <Button type="submit">Update Product</Button>
-      </Form>
-    </Container>
-   </div>
-  );
-};
+    <div>
+      <AdminNavabar/>
+    <br />
+    <h1 className='text-center'>Users</h1> <br />
+    <div style={{display:"flex",justifyContent:"center"}}>
+    <Table striped bordered hover variant="dark" style={{width:"70%"}}>
+      <thead>
+        <tr>
+          <th>sl/no</th>
+          <th>UserId</th>
+          <th>User name</th>
+          <th>Email</th>
+          <th>Operation</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((item, index) => (
+          <tr key={item._id}>
+            <td>{index + 1}</td>
+            <td>{item._id}</td>
+            <td>{item.username}</td>
+            <td>{item.email}</td>
+            <td>
+              {/* <button style={{ border: 'none', background: 'none' }}>
+                <Link to={`/useredit/${item._id}`} style={{ color: 'blue', textDecoration: 'none' }}>
+                  <FaEdit />
+                </Link>
+              </button> */}
+              <button onClick={() => deleteData(item._id)} style={{ border: 'none', color: 'red', background: 'none' }}>
+                <FaTrash />
+              </button>{' '}
+              <Button onClick={() => fetchUserBikeData(item._id)} style={{ marginBottom: '12px' }}>
+                view
+              </Button>
+              <div style={{ display: 'flex' }}>
+                {showDetails && (
+                  <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center z-50" >
+                    <div className="bg-gray-900 bg-opacity-50 absolute inset-0"></div>
+                    <div className="bg-white p-4 rounded-lg z-10 relative" style={{ maxHeight: "80vh", overflowY: "scroll" }}>
+                      {/* Rest of your content */}
+                      <p className="text-sm text-gray-600">
+                        <div className="container mx-auto mt-8" style={{width:"1350px"}}>
+                          <h1 className='text-center text-blue-300'>User Bookings</h1>
+                          {userbookings.map((item) => {
+                             const status = calculateStatus(item.date);
+                            return (
+                                <Card
+                                key={item._id}
+                                style={{
+                                  width: '90%',
+                                  marginLeft: '65px',
+                                  backgroundColor: '#fff',
+                                  boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+                                  borderRadius: '8px',
+                                  paddingTop: '15px',
+                                  marginBottom: '35px',
+                                }}
+                              >
+                                <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                                  <div >
+                                  <img src={`http://localhost:7000/organizer/${item?.templeImage}`} style={{ height: "80px",width:"120px" }} /> 
+                                  </div>
+                                  <div>
+                                    <p>Temple Name:</p>
+                                    <p>{item.templeName}</p>
+                                  </div>
+                                  <div>
+                                    <p>Darshan Name:</p>
+                                    <p>{item.darshanName}</p>
+                                  </div>
+                                  <div>
+                                    <p>Bookingid:</p>
+                                    <p>{item._id.slice(0,10)}</p>
+                                  </div>
+                                  <div>
+                                    <p>Organizer</p>
+                                    <p>{item.organizerName}</p>
+                                  </div>
+                                  <div>
+                                    <p>BookingDate</p>
+                                    <p>{item.BookingDate}</p>
+                                  </div>
+                                  <div>
+                                    <p>Timings</p>
+                                    <p>{item.open}-{item.close}</p>
+                                  </div>
+                                  <div>
+                                    <p>Quantity</p>
+                                    <p>{item.quantity}</p>
+                                  </div>
+                                  <div>
+                                    <p>Price</p>
+                                    <p>{item.totalamount}</p>
+                                  </div>
+                                  <div>
+                                    <p>Status</p>
+                                    <p>{status}</p>
+                                  </div>
+                                  <button onClick={() => deleteorder(item._id)} style={{ border: 'none', color: 'red', background: 'none' }}>
+                <FaTrash />
+              </button>
+                                </div>
+                              </Card>
+                            );
+                          })}
+                        </div>
+                      </p>
+                      <Button onClick={toggleDetails} className="mt-4">
+                        Close
+                      </Button>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+    </div>
+  </div>
+  
+  )
+}
 
-export default UpdateProduct;
+export default Users
