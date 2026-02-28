9d8a817b4449228d2cdb806dd5cd2095c6d89035
rupa695 <2022cse.r454@svce.edu.in>
2026-02-28 12:23:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,97 +1,77 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { Link, NavLink, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Cookies from 'js-cookies';
-import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Header from '../Header';
 
-const Header = () => {
-    const [isAdmin, setIsAdmin] = useState(false);
-    const token = Cookies.getItem("jwtToken");
-    const adminToken = localStorage.getItem("adminJwtToken");
+// Styled components
+const Container = styled.div`
+  padding: 20px;
+  margin-top: 10vh;
+  text-align: start;
+`;
 
-    useEffect(() => {
-        if (adminToken) {
-            setIsAdmin(true);
-        } else {
-            setIsAdmin(false);
-        }
-    }, [adminToken]);
+const Heading = styled.h2`
+  font-size: 24px;
+  margin-bottom: 16px;
+`;
 
-    const navigate = useNavigate();
+const OrderList = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
 
-    const onLogout = () => {
-        Cookies.removeItem('adminJwtToken');
-        const res = window.confirm("Are you sure you want to log out?");
-        if (res) {
-            localStorage.clear();
-            Cookies.removeItem('jwtToken');
-            navigate('/login');
-        }
-    };
+const OrderItem = styled.li`
+  border: 1px solid #ccc;
+  padding: 16px;
+  margin-bottom: 16px;
+`;
 
+const Strong = styled.strong`
+  font-weight: bold;
+`;
 
+const History = () => {
+  const userId = Cookies.getItem('userId');
+  const [orders, setOrders] = useState([]);
 
-    return (
-        <div>
-            {isAdmin ?
-                <Navbar fixed="top" style={{ padding: '0 20px', minHeight: '10vh', width: '100%',backgroundColor:"red" }} expand="lg" bg="light" variant="light">
-                    <Navbar.Brand><Link to='/admin/dashboard' style={{ textDecoration: 'none' }}>G-Mart</Link></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarSupportedContent" />
-                    <Navbar.Collapse id="navbarSupportedContent">
-                        <Nav className="mr-auto">
-                            <NavLink to="/admin/dashboard" className="nav-link">Home</NavLink>
-                            <NavLink to="/admin/all-products" className="nav-link">Products</NavLink>
-                            <NavLink to="/admin/orders" className="nav-link">Orders</NavLink>
-                            <NavLink to="/admin/users" className="nav-link">Users</NavLink>
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5100/my-orders/${userId}`)
+      .then((response) => {
+        // Assuming response.data is an array of orders
+        setOrders(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching orders:', error);
+      });
+  }, [userId]); // Include userId in the dependency array to re-fetch orders when it changes
 
-                            {!adminToken ? (
-                                <NavLink as={NavLink} to="/login" className="nav-link">
-                                    Login/SignUp
-                                </NavLink>
-                            ) : (
-                                <NavLink className="nav-link" to="/login" onClick={onLogout}>
-                                    Logout
-                                </NavLink>
-                            )}
-                            <NavDropdown title="Dropdown" id="navbarDropdown">
-                                <NavDropdown.Item href="#">Action</NavDropdown.Item>
+  return (
+    <div>
+      <Header/>
+      <Container>
+      <h1 className='text-center'>My History</h1>
+      <OrderList>
+        {orders.map((order) => {
+          const isDelivered = order.status === 'Delivered' || order.status === 'Canceled';
 
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#">Something else here</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar> :
-                <Navbar fixed="top" style={{ padding: '0 20px', minHeight: '10vh', width: '100%',backgroundColor:"#34D399" }} expand="lg" >
-                    <Navbar.Brand><Link to='/' style={{ textDecoration: 'none' }}>G-Mart</Link></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarSupportedContent" />
-                    <Navbar.Collapse id="navbarSupportedContent">
-                        <Nav className="mr-auto">
-                            <NavLink to="/" className="nav-link">Home</NavLink>
-                            <NavLink to="/my-cart" className="nav-link">MyCart</NavLink>
-                            <NavLink to="/my-orders" className="nav-link">Orders</NavLink>
-                            <NavLink to="/my-history" className="nav-link">History</NavLink>
-
-                            {!token ? (
-                                <div style={{display:"flex"}}>
-                                    <NavLink as={NavLink} to="/login" className="nav-link">
-                                  User Login 
-                                </NavLink> 
-                                <span className="nav-link">/</span>
-                                <NavLink as={NavLink} to="/alogin" className="nav-link">
-                                    Admin Login
-                                </NavLink>
-                                </div>
-                            ) : (
-                                <NavLink className="nav-link" to="/login" onClick={onLogout}>
-                                    Logout
-                                </NavLink>
-                            )}
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>}
-        </div>
-    );
+          return isDelivered ? (
+            <OrderItem key={order._id} style={{ border: order.status === 'Delivered' ? '1px solid green' : '1px solid red' }}>
+              <Strong>Order ID:</Strong> {order._id} <br />
+              <Strong>Name:</Strong> {order.firstname} {order.lastname} <br />
+              <Strong>Phone:</Strong> {order.phone} <br />
+              <Strong>Date:</Strong> {order.createdAt} <br />
+              <Strong>Price:</Strong> {order.price} <br />
+              <Strong>Status:</Strong> {order.status} <br />
+              <Strong>Payment Method:</Strong> {order.paymentMethod} <br />
+            </OrderItem>
+          ) : null;
+        })}
+      </OrderList>
+    </Container>
+    </div>
+  );
 };
 
-export default Header;
+export default History;
