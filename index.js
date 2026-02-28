d1582f7b9046add43475e4ab54fd075fae63c2c3
rupa695 <2022cse.r454@svce.edu.in>
2026-02-28 12:23:53 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,77 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Cookies from 'js-cookies';
-import styled from 'styled-components';
+
+import React from 'react';
+import { HomeContainer,Container,CenteredRow,ContentColumn,Heading ,Paragraph,PrimaryButton} from "./styledComponents";
+import { Link } from 'react-router-dom';
+import Footer from '../Footer';
+import About from '../About';
+import ContactUs from '../Contact';
 import Header from '../Header';
 
-// Styled components
-const Container = styled.div`
-  padding: 20px;
-  margin-top: 10vh;
-  text-align: start;
-`;
-
-const Heading = styled.h2`
-  font-size: 24px;
-  margin-bottom: 16px;
-`;
-
-const OrderList = styled.ul`
-  list-style: none;
-  padding: 0;
-`;
-
-const OrderItem = styled.li`
-  border: 1px solid #ccc;
-  padding: 16px;
-  margin-bottom: 16px;
-`;
-
-const Strong = styled.strong`
-  font-weight: bold;
-`;
-
-const History = () => {
-  const userId = Cookies.getItem('userId');
-  const [orders, setOrders] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(`http://localhost:5100/my-orders/${userId}`)
-      .then((response) => {
-        // Assuming response.data is an array of orders
-        setOrders(response.data);
-      })
-      .catch((error) => {
-        console.error('Error fetching orders:', error);
-      });
-  }, [userId]); // Include userId in the dependency array to re-fetch orders when it changes
+const Home = () => {
+  const onShop = () => {
+    // Add your logic for the "Shop Now" button click
+    console.log('Shop Now clicked');
+  };
 
   return (
     <div>
       <Header/>
+      <HomeContainer className="home-container" src="https://img.freepik.com/free-vector/online-grocery-store-banner-design_23-2150089535.jpg?size=626&ext=jpg&ga=GA1.1.1700460183.1712534400&semt=ais">
       <Container>
-      <h1 className='text-center'>My History</h1>
-      <OrderList>
-        {orders.map((order) => {
-          const isDelivered = order.status === 'Delivered' || order.status === 'Canceled';
-
-          return isDelivered ? (
-            <OrderItem key={order._id} style={{ border: order.status === 'Delivered' ? '1px solid green' : '1px solid red' }}>
-              <Strong>Order ID:</Strong> {order._id} <br />
-              <Strong>Name:</Strong> {order.firstname} {order.lastname} <br />
-              <Strong>Phone:</Strong> {order.phone} <br />
-              <Strong>Date:</Strong> {order.createdAt} <br />
-              <Strong>Price:</Strong> {order.price} <br />
-              <Strong>Status:</Strong> {order.status} <br />
-              <Strong>Payment Method:</Strong> {order.paymentMethod} <br />
-            </OrderItem>
-          ) : null;
-        })}
-      </OrderList>
-    </Container>
+        
+        <CenteredRow>
+          <ContentColumn>
+            {/* <img src='https://img.freepik.com/free-vector/online-grocery-store-banner-design_23-2150089535.jpg?size=626&ext=jpg&ga=GA1.1.1700460183.1712534400&semt=ais' /> */}
+            <Heading>Welcome to Our Grocery Web App</Heading>
+            <Paragraph>Discover a wide range of Grocery's and Fresh Items for all your needs</Paragraph>
+            <PrimaryButton> <Link to='/shopping' style={{textDecoration:'none',color:'white',fontWeight:'bolder'}}>Shop Now</Link> </PrimaryButton>
+          </ContentColumn>
+        </CenteredRow>
+      </Container>
+    </HomeContainer>
+    <About/>
+    <ContactUs/>
+    <Footer/>
     </div>
   );
-};
+}
 
-export default History;
+export default Home;
